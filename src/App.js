09fa907f1aef9f6595899795121982386e76aca9
nicocran1911 <nicholas.ocran@amalitech.org>
2022-08-09 21:47:12 +0000

    convected to json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
 const BASE_URL = "https://api.exchangeratesapi.io/latest";
 
+
 function App() {
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, []);
+
   return (
     <>
       <h1>Currency Converter</h1>
