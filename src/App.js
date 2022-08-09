def96077542407c969d4f524c286936ae5b63153
nicocran1911 <nicholas.ocran@amalitech.org>
2022-08-09 21:59:35 +0000

    setup default currency

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ const BASE_URL = "https://api.exchangeratesapi.io/latest";
 function App() {
 
   const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
-        setCurrencyOptions([data.base, Object.keys()])
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
       })
   }, []);
 
   return (
     <>
       <h1>Currency Converter</h1>
-      <CurrencyRow />
+      <CurrencyRow 
+        currencyOptions={curre}
+      />
       <div className="equals">=</div>
       <CurrencyRow />
     </>
