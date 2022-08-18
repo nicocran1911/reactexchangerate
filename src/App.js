25dc45899dc0119af2710db264ace3694e1b1e23
nicocran1911 <nicholas.ocran@amalitech.org>
2022-08-18 13:13:16 +0000

    calling the api

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import 'react-dropdown/style.css';
 import './App.css';
 
 function App() {
+  // Initializing all the state variables
+  const [info, setInfo] = useState([]);
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("GBP");
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
 
-// Initializing all the state variables
-const [info, setInfo] = useState([]);
-const [input, setInput] = useState(0);
-const [from, setFrom] = useState("USD");
-const [to, setTo] = useState("GBP");
-const [options, setOptions] = useState([]);
-const [output, setOutput] = useState(0);
+  // Calling the api whenever the dependency changes
+  useEffect(() => {
+    Axios.get(
+      `https://v6.exchangerate-api.com/v6/6c40a6d1f6b4576d0fb50f55/latest/USD`
+    ).then((res) => {
+      const data = res.data.conversion_rates;
+      setInfo(data);
+    });
+  }, [from]);
 
+  // Calling the convert function whenever
+  // a user switches the currency
+  useEffect(() => {
+    setOptions(Object.keys(info));
+    convert();
+  }, [info]);
+
+ 
