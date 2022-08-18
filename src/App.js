aecc8c53723b998cfbc10c382796bf561dc382ca
nicocran1911 <nicholas.ocran@amalitech.org>
2022-08-18 13:10:05 +0000

    updated files, set all state variables

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
 import './App.css';
-import CurrencyRow from './CurrencyRow';
-
-const BASE_URL = "https://v6.exchangerate-api.com/v6/6c40a6d1f6b4576d0fb50f55/latest/USD";
-
-// const bgImage = new URL("../public/Web 19201.png",import.meta.url )
 
 function App() {
 
-  const [currencyOptions, setCurrencyOptions] = useState([])
-  const [fromCurrency, setFromCurrency] = useState()
-  const [toCurrency, setToCurrency] = useState()
-  const [exchangeRate, setExchangeRate] =useState()
-  const [amount, setAmount] = useState(1)
-  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+// Initializing all the state variables
+const [info, setInfo] = useState([]);
+const [input, setInput] = useState(0);
+const [from, setFrom] = useState("USD");
+const [to, setTo] = useState("GBP");
+const [options, setOptions] = useState([]);
+const [output, setOutput] = useState(0);
 
-  let toAmount, fromAmount;
-
-  if (amountInFromCurrency) {
-    fromAmount = amount
-    toAmount = amount * exchangeRate
-      console.log(toAmount);
-  } else {
-    toAmount = amount
-    fromAmount = amount / exchangeRate
-  }
-
-  useEffect(() => {
-    fetch(BASE_URL)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        const firstCurrency = Object.keys(data.rates)[0]
-        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-        setFromCurrency(data.base)
-        setToCurrency(firstCurrency)
-        setExchangeRate(data.rates[firstCurrency])
-      })
-  }, []);
-
-
-  useEffect(() => {
-    if (fromCurrency != null && toCurrency != null ) {
-    fetch(`${BASE_URL}?base=${fromCurrency}&symbols=&{toCurrency}`)
-    .then(res => res.json())
-    .then(data => setExchangeRate(data.rates[toCurrency]))
-    }
-  }, [fromCurrency, toCurrency])
-
-  function handleFromAmountChange(e) {
-    setAmount(e.target.value)
-    setAmountInFromCurrency(true)
-  }
-
-  function handleToAmountChange(e) {
-    setAmount(e.target.value);
-    setAmountInFromCurrency(false);
-  }
-  
-
-  return (
-    <>
-      <div className='wrapper'>
-        <h1> Convert</h1>
-        <CurrencyRow
-          currencyOptions={currencyOptions}
-          selectedCurrency={fromCurrency}
-          onChangeCurrency={(e) => setToCurrency(e.target.value)}
-          onChangeAmount={handleFromAmountChange}
-          amount={fromAmount}
-        />
-        <div className="equals">=</div>
-        {/* <img src={bgImage}  /> */}
-        <CurrencyRow
-          currencyOptions={currencyOptions}
-          selectedCurrency={toCurrency}
-          onChangeCurrency={(e) => setToCurrency(e.target.value)}
-          onChangeAmount={handleToAmountChange}
-          amount={toAmount}
-        />
-      </div>
-    </>
-  );
-}
-
-export default App;
